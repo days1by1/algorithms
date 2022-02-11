4566bfd0700937102186bfee49891aa62db8f679
days1by1 <o8diRS3XNwfE>
2022-02-11 09:06:49 +0800

    除以2和右移1位的结果相同

--- src/main/java/leetcode/coding_interviews/T13CuttingRope2.java
+++ src/main/java/leetcode/coding_interviews/T13CuttingRope2.java
@@ -15,9 +15,9 @@ public class T13CuttingRope2 {
         System.out.println(cuttingRope(10));
         System.out.println(cuttingRope(59));
 
-        System.out.println("==== 注意：除以2和右移2位的结果并不相同！！！ ====");
+        System.out.println("==== 注意：除以2和右移1位的结果相同！！！ ====");
         System.out.println(9 / 2);
-        System.out.println(9 >> 2);
+        System.out.println(9 >> 1);
     }
 
     /**
@@ -49,8 +49,8 @@ public class T13CuttingRope2 {
         while (a > 0) {
             if (a % 2 == 1) rem = (rem * X) % p;
             X = (X * X) % p;
-            a /= 2;
-            // a >>= 2;    // 此处需要除以2再向下取整，不能使用右移
+            // a /= 2;
+            a >>= 1;
         }
         return (int)rem;
     }
@@ -63,12 +63,12 @@ public class T13CuttingRope2 {
         int b = n % 3, p = 1000000007;
         long rem = 1, x = 3;
 
-        // for (int a = n / 3 - 1; a > 0; a /= 2) {
-        //     if (a % 2 == 1) rem = (rem * x) % p;
-        //     x = (x * x) % p;
-        // }
+        for (int a = n / 3 - 1; a > 0; a /= 2) {
+            if (a % 2 == 1) rem = (rem * x) % p;
+            x = (x * x) % p;
+        }
         // rem = loopRemainder((int) x, n / (int) x - 1, p);
-        rem = fastPowerRemainder((int) x, n / (int) x - 1, p);
+        // rem = fastPowerRemainder((int) x, n / (int) x - 1, p);
 
         if (b == 0) return (int) (rem * 3 % p);
         if (b == 1) return (int) (rem * 4 % p);
