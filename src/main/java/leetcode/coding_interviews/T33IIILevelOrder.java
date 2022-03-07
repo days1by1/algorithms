26e141cf58da82cc7de0a5e3eac0823a75abe2e0
days1by1 <o8diRS3XNwfE>
2022-03-07 10:24:43 +0800

    剑指Offer 32-III其他方法

--- src/main/java/leetcode/coding_interviews/T33IIILevelOrder.java
+++ src/main/java/leetcode/coding_interviews/T33IIILevelOrder.java
@@ -2,7 +2,13 @@ package leetcode.coding_interviews;
 
 import org.junit.Test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
 
 /**
  * 剑指 Offer 32 - III. 从上到下打印二叉树 III
@@ -20,8 +26,11 @@ public class T33IIILevelOrder {
         root.right = new TreeNode(3);
         root.left.left = new TreeNode(4);
         root.right.right = new TreeNode(5);
-        List<List<Integer>> lists = levelOrder(root);
-        lists.forEach(System.out::println);
+
+        levelOrder(root).forEach(System.out::println);
+        levelOrder2(root).forEach(System.out::println);
+        levelOrder3(root).forEach(System.out::println);
+        levelOrder4(root).forEach(System.out::println);
     }
 
     /**
@@ -55,6 +64,85 @@ public class T33IIILevelOrder {
                 }
             }
             res.add(temp);
+        }
+        return res;
+    }
+
+    /**
+     * Krahets解答：双端队列。
+     * 把链表LinkedList作为双端队列使用
+     */
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root != null) queue.add(root);
+        while (!queue.isEmpty()) {
+            LinkedList<Integer> tmp = new LinkedList<>();
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                if ((res.size() & 1) == 0) tmp.addLast(node.val);
+                else tmp.addFirst(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            res.add(tmp);
+        }
+        return res;
+    }
+
+    /**
+     * Krahets解答：双端队列（奇偶层逻辑分离）
+     * 使用双端队列Deque，奇偶层逻辑分离减少冗余判断
+     */
+    public List<List<Integer>> levelOrder3(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        Deque<TreeNode> deque = new LinkedList<>();
+        if (root != null) deque.add(root);
+        while (!deque.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            // 打印奇数层
+            for (int i = deque.size(); i > 0; i--) {
+                // 从左向右打印
+                TreeNode node = deque.removeFirst();
+                tmp.add(node.val);
+                // 先左后右加入下层节点
+                if (node.left != null) deque.addLast(node.left);
+                if (node.right != null) deque.addLast(node.right);
+            }
+            res.add(tmp);
+            if (deque.isEmpty()) break;  // 若为空则提前跳出
+            // 打印偶数层
+            tmp = new ArrayList<>();
+            for (int i = deque.size(); i > 0; i--) {
+                // 从右向左打印
+                TreeNode node = deque.removeLast();
+                tmp.add(node.val);
+                // 先右后左加入下层节点
+                if (node.right != null) deque.addFirst(node.right);
+                if (node.left != null) deque.addFirst(node.left);
+            }
+            res.add(tmp);
+        }
+        return res;
+    }
+
+    /**
+     * Krahets解答：倒序
+     */
+    public List<List<Integer>> levelOrder4(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root != null) queue.add(root);
+        while (!queue.isEmpty()) {
+            List<Integer> tmp = new ArrayList<>();
+            for (int i = queue.size(); i > 0; i--) {
+                TreeNode node = queue.poll();
+                tmp.add(node.val);
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+            if (res.size() % 2 == 1) Collections.reverse(tmp);
+            res.add(tmp);
         }
         return res;
     }
