a742bb5361698101dda12d8e0cb3abe1593116e4
days1by1 <o8diRS3XNwfE>
2022-03-08 23:26:04 +0800

    剑指Offer 33 方法二：辅助单调栈

--- src/main/java/leetcode/coding_interviews/T33VerifyPostorder.java
+++ src/main/java/leetcode/coding_interviews/T33VerifyPostorder.java
@@ -1,5 +1,9 @@
 package leetcode.coding_interviews;
 
+import org.junit.Test;
+
+import java.util.Stack;
+
 /**
  * 剑指 Offer 33. 二叉搜索树的后序遍历序列
  * 难度：中等
@@ -8,6 +12,15 @@ package leetcode.coding_interviews;
  * @since 2022年03月07日 13:31
  */
 public class T33VerifyPostorder {
+
+    @Test
+    public void test() {
+        // 题目给的条件是二叉搜索树，只是看是否是后序遍历序列，
+        // 因此划分左右子树的方法是从左找第一个大于根节点的节点
+        System.out.println(verifyPostorder(new int[]{4, 3, 2}));    // 看作是没有左子树
+        System.out.println(verifyPostorder(new int[]{1, 3, 2, 6, 5}));
+        System.out.println(verifyPostorder2(new int[]{1, 3, 2, 6, 5}));
+    }
 
     /**
      * Krahets解答：递归分治
@@ -29,6 +42,14 @@ public class T33VerifyPostorder {
      * Krahets参考「失火的夏天」的解答：辅助单调栈
      */
     public boolean verifyPostorder2(int[] postorder) {
+        Stack<Integer> stack = new Stack<>();
+        int root = Integer.MAX_VALUE;
+        for (int i = postorder.length - 1; i >= 0; i--) {
+            if (postorder[i] > root) return false;
+            while (!stack.isEmpty() && stack.peek() > postorder[i])
+                root = stack.pop();
+            stack.add(postorder[i]);
+        }
         return true;
     }
 }
