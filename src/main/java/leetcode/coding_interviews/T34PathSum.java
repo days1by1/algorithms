51a2368d7284f92d6086b0b2749e73c263530050
days1by1 <o8diRS3XNwfE>
2022-03-10 10:31:15 +0800

    剑指Offer 34：BFS

--- src/main/java/leetcode/coding_interviews/T34PathSum.java
+++ src/main/java/leetcode/coding_interviews/T34PathSum.java
@@ -1,9 +1,6 @@
 package leetcode.coding_interviews;
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 剑指 Offer 34. 二叉树中和为某一值的路径
@@ -26,7 +23,9 @@ public class T34PathSum {
     }
 
     public void dfs(TreeNode root, int target) {
-        if (root == null) return;
+        if (root == null) {
+            return;
+        }
         path.offerLast(root.val);
         target -= root.val;
         if (root.left == null && root.right == null && target == 0) {
@@ -40,5 +39,55 @@ public class T34PathSum {
         dfs(root.left, target);
         dfs(root.right, target);
         path.pollLast();
+    }
+
+    /**
+     * 官方解答：BFS
+     */
+    List<List<Integer>> ret = new LinkedList<>();
+    Map<TreeNode, TreeNode> map = new HashMap<>();
+
+    public List<List<Integer>> pathSum2(TreeNode root, int target) {
+        if (root == null) {
+            return ret;
+        }
+        Queue<TreeNode> queueNode = new LinkedList<>();
+        Queue<Integer> queueSum = new LinkedList<>();
+        queueNode.offer(root);
+        queueSum.offer(0);
+
+        while (!queueNode.isEmpty()) {
+            TreeNode node = queueNode.poll();
+            Integer rec = queueSum.poll() + node.val;
+
+            if (node.left == null && node.right == null) {
+                if (rec == target) {
+                    getPath(node);
+                }
+            } else {
+                if (node.left != null) {
+                    map.put(node.left, node);
+                    queueNode.offer(node.left);
+                    queueSum.offer(rec);
+                }
+                if (node.right != null) {
+                    map.put(node.right, node);
+                    queueNode.offer(node.right);
+                    queueSum.offer(rec);
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    public void getPath(TreeNode node) {
+        List<Integer> tmp = new LinkedList<>();
+        while (node != null) {
+            tmp.add(node.val);
+            node = map.get(node);
+        }
+        Collections.reverse(tmp);
+        ret.add(tmp);
     }
 }
